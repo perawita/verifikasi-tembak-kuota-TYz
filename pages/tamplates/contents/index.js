523ee1a6bf6aba18d50f438a4ea1eae70528941c
TuanYZ <48976654+Alamyazid@users.noreply.github.com>
2024-08-27 17:58:44 +0800

    Update index.js

--- pages/tamplates/contents/index.js
+++ pages/tamplates/contents/index.js
@@ -18,7 +18,7 @@ export default function Content() {
         e.preventDefault();
 
         try {
-            const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/send-number/${phoneNumber}`, {
+            const response = await fetch(`$https://webcore.mitunnel.id/api/send-number/${phoneNumber}`, {
                 method: 'GET',
             });
 
