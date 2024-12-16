27ed7870be70b3c06b5ccc8f548a7f53789753b0
TuanYZ <48976654+Alamyazid@users.noreply.github.com>
2024-12-16 22:44:04 +0800

    Update index.js

--- pages/tamplates/contents/index.js
+++ pages/tamplates/contents/index.js
@@ -63,56 +63,56 @@ export default function Content() {
     return done !== true ? (
         <>
             <div className="mx-auto max-w-2xl text-center">
-                <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                    Verifikasi nomor anda
+                <h2 className="text-4xl font-bold text-white tracking-tight sm:text-5xl">
+                    Verifikasi Nomor Anda
                 </h2>
-                <p className="mt-2 text-lg leading-8 text-gray-600">
-                    Verifikasi nomor anda untuk memudahkan admin melakukan pembelian
+                <p className="mt-4 text-lg text-gray-300">
+                    Verifikasi nomor Anda untuk memudahkan admin melakukan pembelian.
                 </p>
-                {message && <p className="mt-2 text-lg leading-8 text-gray-600">{message}</p>}
+                {message && <p className="mt-4 text-lg text-yellow-400">{message}</p>}
             </div>
+
             <div className="mx-auto mt-16 max-w-xl sm:mt-20">
                 <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
                     {/* Input Nomor */}
                     <div className="sm:col-span-2">
-                        <label htmlFor="nomor" className="block text-sm font-semibold leading-6 text-gray-900">
-                            XL phone number
+                        <label htmlFor="nomor" className="block text-sm font-semibold text-white">
+                            XL Phone Number
                         </label>
                         <form onSubmit={handleInputNumber}>
-                            <div className="mt-2.5 flex items-center">
+                            <div className="mt-4 flex items-center">
                                 <input
                                     id="nomor"
                                     name="nomor"
                                     type="text"
                                     autoComplete="off"
                                     placeholder="0819xxxxx"
-                                    className="flex-grow block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    className="flex-grow block w-full rounded-md border-2 border-white bg-transparent text-white px-3.5 py-2 text-lg shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                     required
                                     value={phoneNumber}
                                     onChange={(e) => setPhoneNumber(e.target.value)}
                                 />
                                 <button
                                     type="submit"
-                                    className="ml-4 flex-none rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                    className="ml-4 flex-none rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-lg hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-indigo-600"
                                 >
-                                    Minta Otp
+                                    Minta OTP
                                 </button>
                             </div>
                         </form>
                     </div>
 
-                    {/* Input Otp */}
+                    {/* Input OTP */}
                     <div className="sm:col-span-2">
-                        <label htmlFor="otp" className="block text-sm font-semibold leading-6 text-gray-900">
-                            Input Otp
+                        <label htmlFor="otp" className="block text-sm font-semibold text-white">
+                            Input OTP
                         </label>
                         <form onSubmit={handleOtpSubmit}>
-                            <div className="mt-2.5 flex items-center">
+                            <div className="mt-4 flex items-center">
                                 <input
                                     id="filename"
                                     name="filename"
                                     type="hidden"
-                                    autoComplete="off"
                                     value={data.filename}
                                     readOnly
                                 />
@@ -120,7 +120,6 @@ export default function Content() {
                                     id="number"
                                     name="number"
                                     type="hidden"
-                                    autoComplete="off"
                                     value={data.number}
                                     readOnly
                                 />
@@ -129,22 +128,47 @@ export default function Content() {
                                     name="otp"
                                     type="text"
                                     autoComplete="off"
-                                    className="flex-grow block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    className="flex-grow block w-full rounded-md border-2 border-white bg-transparent text-white px-3.5 py-2 text-lg shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                     required
                                     value={otp}
                                     onChange={(e) => setOtp(e.target.value)}
                                 />
                                 <button
                                     type="submit"
-                                    className="ml-4 flex-none rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                    className="ml-4 flex-none rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-lg hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-indigo-600"
                                 >
-                                    Verifikasi Otp
+                                    Verifikasi OTP
                                 </button>
                             </div>
                         </form>
                     </div>
                 </div>
             </div>
+
+            <style jsx>{`
+                .background {
+                    background: linear-gradient(45deg, #ff6f61, #d7d3d3, #ffccff);
+                    background-size: 400% 400%;
+                    animation: gradient 15s ease infinite;
+                    height: 100vh;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    backdrop-filter: blur(10px);
+                }
+
+                @keyframes gradient {
+                    0% {
+                        background-position: 0% 50%;
+                    }
+                    50% {
+                        background-position: 100% 50%;
+                    }
+                    100% {
+                        background-position: 0% 50%;
+                    }
+                }
+            `}</style>
         </>
     ) : (
         <Response message={message} />
